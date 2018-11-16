976d14d8e9c7b785b348fae4019133cf3b9ec720
Kayla Dutcher <kayd@live.unc.edu>
2018-11-16 18:22:22 -0500

    commit to submit tests

--- src/a6test/kayd/A6Tests.java
+++ src/a6test/kayd/A6Tests.java
@@ -8,55 +8,42 @@ package a6test.kayd;
 import a6Tests.ROIObserverImpl;
 
 public class A6Tests {
-		Pixel red = new ColorPixel(1.0,0.0,0.0);
-		Pixel green = new ColorPixel(0.0,1.0,0.0);
-		Pixel blue = new ColorPixel(0.0,0.0,1.0);
-		Pixel yellow = new ColorPixel(1.0,1.0,0.0);
-		Pixel cyan = new ColorPixel(0.0,1.0,1.0);
-		Pixel magenta = new ColorPixel(1.0,0.0,1.0);
-		Pixel white = new ColorPixel(1.0,1.0,1.0);
-		Pixel black = new ColorPixel(0.0,0.0,0.0);
-		Pixel randomOne = new ColorPixel(0.5,0.6,0.7);
-		Pixel randomTwo = new ColorPixel(0.6,0.8,0.1);
-		Pixel randomThree = new ColorPixel(0.7,0.1,0.3);
-		Pixel randomFour = new ColorPixel(0.4,0.6,0.1);
-		Pixel randomFive = new ColorPixel(0.5,0.3,0.9);
-		Pixel randomSix = new ColorPixel(0.1,0.2,0.4);
-		Pixel randomSeven = new ColorPixel(0.1,0.7,0.3);
-		Pixel randomEight = new ColorPixel(0.4,0.5,0.3);
-		Pixel randomNine = new ColorPixel(0.1,0.15,0.6);	
-		Pixel[][] randomArrayOne = {{randomOne, randomTwo, randomThree},
-				 					{randomFour, randomFive, randomSix},
-				 					{randomSeven, randomEight, randomNine}};
-		Pixel[][] largeRandomArray = {{randomOne, randomTwo, randomThree, randomFour},
-									  {randomFive, randomSix, randomSeven, randomEight},
-									  {randomNine, randomOne, randomTwo, randomThree},
-									  {randomFour, randomFive, randomSix, randomSeven}};
-		Pixel[][] largeArray = {{red, green, blue, yellow},
-								{cyan, magenta, white, black},
-								{red, green, blue, yellow},
-								{cyan, magenta, white, black}};
-		Pixel[][] rectangleArray = {{red, cyan}, 
-									{cyan,magenta}, 
-									{cyan, magenta}, 
-									{cyan, magenta},
-									{magenta, red}};
-		Pixel[][] randomArrayTwo = {{red, green},    
-									{blue, yellow}};
-																											     	// C M M M R
-		
-		Picture randomPictureLarge = new MutablePixelArrayPicture(randomArrayOne, "random picture");
-		Picture squarePicture = new MutablePixelArrayPicture(largeRandomArray, "the larges square");
-		Picture immutableSquare = new ImmutablePixelArrayPicture(largeRandomArray, "the large immutable");
-		Picture largePictureOne = new MutablePixelArrayPicture(largeArray, "large picture made of solid color pixels"); 											 
-		Picture rectanglePicture = new MutablePixelArrayPicture(rectangleArray,"rectangle picture"); 
-		Picture smallRandom = new MutablePixelArrayPicture(randomArrayTwo, "It's a little random");
+	Pixel red = new ColorPixel(1.0,0.0,0.0);
+	Pixel green = new ColorPixel(0.0,1.0,0.0);
+	Pixel blue = new ColorPixel(0.0,0.0,1.0);
+	Pixel yellow = new ColorPixel(1.0,1.0,0.0);
+	Pixel cyan = new ColorPixel(0.0,1.0,1.0);
+	Pixel magenta = new ColorPixel(1.0,0.0,1.0);
+	Pixel white = new ColorPixel(1.0,1.0,1.0);
+	Pixel black = new ColorPixel(0.0,0.0,0.0);
+	Pixel randomOne = new ColorPixel(0.5,0.6,0.7);
+	Pixel randomTwo = new ColorPixel(0.6,0.8,0.1);
+	Pixel randomThree = new ColorPixel(0.7,0.1,0.3);
+	Pixel randomFour = new ColorPixel(0.4,0.6,0.1);
+	Pixel randomFive = new ColorPixel(0.5,0.3,0.9);
+	Pixel randomSix = new ColorPixel(0.1,0.2,0.4);
+	Pixel randomSeven = new ColorPixel(0.1,0.7,0.3);
+	Pixel randomEight = new ColorPixel(0.4,0.5,0.3);
+	Pixel randomNine = new ColorPixel(0.1,0.15,0.6);	
+	Pixel[][] randomArrayOne = {{randomOne, randomTwo, randomThree},
+			 					{randomFour, randomFive, randomSix},
+			 					{randomSeven, randomEight, randomNine}};
+	Pixel[][] largeRandomArray = {{randomOne, randomTwo, randomThree, randomFour},
+								  {randomFive, randomSix, randomSeven, randomEight},
+								  {randomNine, randomOne, randomTwo, randomThree},
+								  {randomFour, randomFive, randomSix, randomSeven}};
+	Pixel[][] randomArrayTwo = {{red, green},    
+								{blue, yellow}};
+																										     	// C M M M R
+	
+	Picture randomPictureLarge = new MutablePixelArrayPicture(randomArrayOne, "random picture");
+	Picture immutableSquare = new ImmutablePixelArrayPicture(largeRandomArray, "the large immutable");
+	Picture smallRandom = new MutablePixelArrayPicture(randomArrayTwo, "It's a little random");
 
-		Picture random = new ObservablePictureImpl(randomPictureLarge);
-		Picture square = new ObservablePictureImpl(squarePicture);
-		Picture largePicture = new ObservablePictureImpl(largePictureOne);
-		Picture rectangle = new ObservablePictureImpl(rectanglePicture);
-		Picture immutable = new ObservablePictureImpl(immutableSquare);
+	ObservablePicture random = new ObservablePictureImpl(randomPictureLarge);
+	ObservablePicture immutable = new ObservablePictureImpl(immutableSquare);
+	ObservablePicture smallRandomObserve = new ObservablePictureImpl(smallRandom);
+	
 		Region one = new RegionImpl(1,3,3,4);
 		Region two = new RegionImpl(0,0,4,4);
 		Region intersectRegion = new RegionImpl(1,3,3,4);
